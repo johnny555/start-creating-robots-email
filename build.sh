22198046cef6494255542d1501fa96d35c1d167f
John Vial <badwolf.johnnyv@gmail.com>
2023-09-08 14:00:22 +0800

    Quality of life fixes.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/.bashrc
+source /opt/ros/humble/local_setup.bash
+source install/setup.bash
+PYTHONWARNINGS="ignore:setup.py install is deprecated::setuptools.command.install,ignore:easy_install command is deprecated::setuptools.command.easy_install"; 
+export PYTHONWARNINGS
+
+colcon build --symlink-install
--- src/start_creating_robots/setup.py
+++ src/start_creating_robots/setup.py
@@ -1,25 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'start_creating_robots'
+package_name = "start_creating_robots"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ros',
-    maintainer_email='badwolf.johnnyv@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="ros",
+    maintainer_email="john@soundelve.com",
+    description="Example Robot System",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
